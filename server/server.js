59693194ea514d1a8193dc93c1c808a6371fb4ad
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-05-16 12:51:56 +0530

    feat:project initiated

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
 import express from "express";
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => res.send(`Server is running on ${port}`));
+
+app.listen(port, () => {
+  console.log(`Server started on port http://localhost:${port}`);
+});
