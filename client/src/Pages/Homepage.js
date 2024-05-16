9c5f04a1c3c744edcafac3f6bcfa1850262e2510
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-05-16 17:58:56 +0530

    feat:fetching data by api started

--- client/src/Pages/Homepage.js
+++ client/src/Pages/Homepage.js
@@ -1,7 +1,21 @@
+import { Box, Container, Text } from "@chakra-ui/react";
 import React from "react";
 
 const Homepage = () => {
-  return <div>Home</div>;
+  return (
+    <Container maxW="xl">
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        marginBlockStart={"50px"}
+        inlineSize={"100%"}
+        bg={"white"}
+      >
+        <Text>Chat Hub</Text>
+      </Box>
+    </Container>
+  );
 };
 
 export default Homepage;
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./index.css";
+
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 
