c2b887d8f511ea5c8fb63cb50cfebdbbf2382e8e
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-05-20 16:28:22 +0530

    feat: image uploaded and toast added

--- client/src/Components/Authentication/Signup.js
+++ client/src/Components/Authentication/Signup.js
@@ -6,6 +6,7 @@ import {
   InputGroup,
   InputRightElement,
   VStack,
+  useToast,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 
@@ -16,11 +17,54 @@ const Signup = () => {
   const [password, setPassword] = useState();
   const [confirmPassword, setConfirmPassword] = useState();
   const [pic, setPic] = useState();
+  const [loading, setLoading] = useState(false);
+  const toast = useToast();
 
   const handleClick = () => setShow(!show);
 
-  const postDetails = (pics) => {};
-
+  const postDetails = (pics) => {
+    setLoading(true);
+    if (pics === undefined) {
+      toast({
+        title: "Please select an image",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+    if (pics.type === "image/jpeg" || pics.type === "image/png") {
+      const data = new FormData();
+      data.append("file", pics);
+      data.append("upload_preset", "chat-app");
+      data.append("cloud_name", "shubhi-cloud");
+      fetch("https://api.cloudinary.com/v1_1/shubhi-cloud/image/upload", {
+        method: "post",
+        body: data,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setPic(data.url.toString());
+          console.log(data.url.toString());
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setLoading(false);
+        });
+    } else {
+      toast({
+        title: "Please select an image",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+      return;
+    }
+  };
   const submitHandler = () => {};
 
   return (
@@ -88,6 +132,7 @@ const Signup = () => {
         width={"100%"}
         marginBlockStart={"25px"}
         onClick={submitHandler}
+        isLoading={loading}
       >
         Sign Up
       </Button>
