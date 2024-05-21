0937ebac5074d9faf623435f2612d55004667155
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-05-21 15:53:13 +0530

    feat : fetch chat added

--- server/interfaces/controllers/chat.controller.js
+++ server/interfaces/controllers/chat.controller.js
@@ -7,14 +7,54 @@ export const createChat = async (req, res) => {
   const { userId, chatName } = req.body;
   if (!userId) throw new Error("UserId is not sent");
 
-  const chat = await Chat.create({
-    chatName: chatName,
+  let existingChat = await Chat.find({
     isGroupChat: false,
-    users: [req.user._id, userId],
+    $and: [
+      {
+        users: { $elemMatch: { $eq: req.user._id } },
+        users: { $elemMatch: { $eq: userId } },
+      },
+    ],
+  })
+    .populate("users", "-password -__v -createdAt -updatedAt ")
+    .populate("latestMessage");
+
+  existingChat = await User.populate(existingChat, {
+    path: "latestMessage.sender",
+    select: "name pic email",
   });
-  const detailedChat = await Chat.findOne({ _id: chat._id }).populate(
-    "users",
-    "-password"
-  );
-  res.json(detailedChat);
+
+  console.log(existingChat);
+
+  if (existingChat.length > 0) {
+    res.status(200).json(existingChat[0]);
+  } else {
+    const chat = await Chat.create({
+      chatName: chatName,
+      isGroupChat: false,
+      users: [req.user._id, userId],
+    });
+    const detailedChat = await Chat.findOne({ _id: chat._id }).populate(
+      "users",
+      "-password -__v -createdAt -updatedAt "
+    );
+    res.json(detailedChat);
+  }
+};
+
+export const fetchChats = async (req, res) => {
+  const chat = await Chat.find({
+    users: { $elemMatch: { $eq: req.user._id } },
+  })
+    .populate("users", "-password -__v -createdAt -updatedAt")
+    .populate("groupAdmin", "-password -__v -createdAt -updatedAt")
+    .populate("latestMessage")
+    .sort({ updated: -1 })
+    .then(async (results) => {
+      results = await User.populate(results, {
+        path: "latestMessage.sender",
+        select: "name profilePic email",
+      });
+      res.status(200).json(results);
+    });
 };
--- server/interfaces/routers/chat.router.js
+++ server/interfaces/routers/chat.router.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { createChat } from "../controllers/chat.controller.js";
+import { createChat, fetchChats } from "../controllers/chat.controller.js";
 
 const router = Router();
 
-router.route("/").post(createChat);
+router.route("/").post(createChat).get(fetchChats);
 
 export default router;
