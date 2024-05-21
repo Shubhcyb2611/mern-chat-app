332bf7852c42911287108416a9509a45f534d499
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-05-21 18:00:23 +0530

    feat : message added

--- server/interfaces/controllers/message.controller.js
+++ server/interfaces/controllers/message.controller.js
@@ -1,1 +1,18 @@
-export const sendMessage = async (req, res) => {};
+import { Chat } from "../../domain/chat";
+import { Message } from "../../domain/message.js";
+
+export const sendMessage = async (req, res) => {
+  let message = await Message.create({
+    content: req.body.content,
+    sender: req.user._id,
+    chat: req.body.chatId,
+  });
+  message = await message.populate("sender", "name profilePic").execPopulate();
+  message = await message.populate("chat").execPopulate();
+  message = await User.populate(message, {
+    path: "chat.users",
+    select: "name pic ",
+  });
+  await Chat.findByIdAndUpdate(req.body.chatId, { latestMessage: message });
+  res.status(200).json(message);
+};
