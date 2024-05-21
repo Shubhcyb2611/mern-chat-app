10b538def0493d1ca6a28dffc41b62ab27d8c8db
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-05-21 11:33:20 +0530

    feat: search of users added

--- server/interfaces/controllers/user.controller.js
+++ server/interfaces/controllers/user.controller.js
@@ -8,7 +8,15 @@ export const registerUser = async (req, res) => {
 };
 
 export const getAllUsers = asyncHandler(async (req, res) => {
-  const users = await User.find({});
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { name: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
   res.status(200).json(users);
 });
 
